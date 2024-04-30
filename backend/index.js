90abafcfa10bebb5ba61e10131563d092041e803
Administrator <nhan.nguyen1@paysmart.com.vn>
2024-05-01 00:56:32 +0700

    6

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import app from './server.js' 
 import mongodb from "mongodb" 
 import dotenv from "dotenv"
+import MoviesDAO from './dao/moviesDAO.js'
+
 async function main(){ 
   dotenv.config()
   const client = new mongodb.MongoClient(
@@ -10,6 +12,7 @@ const port = process.env.PORT || 8000
 try {
 // Connect to the MongoDB cluster 
   await client.connect()
+  await MoviesDAO.injectDB(client)
   app.listen(port, () =>{
     console.log('server is running on port:'+port);
 })
